202d43e73437afd419528aceeb9c7420b5569977
권용현 <rikeey98@naver.com>
2026-08-14 00:55:02 +0900

    feat: expose updateIfUnchanged across the ipc boundary

--- electron/ipc/memos.ts
+++ electron/ipc/memos.ts
@@ -1,11 +1,15 @@
 import { ipcMain } from 'electron'
 import { CHANNELS } from '@shared/channels'
 import type { Core } from '@core/index'
-import type { CreateMemoInput, UpdateMemoInput, ListQuery } from '@shared/models'
+import type { CreateMemoInput, UpdateMemoInput, GuardedUpdateMemoInput, ListQuery } from '@shared/models'
 
 export function registerMemoHandlers(core: Core) {
   ipcMain.handle(CHANNELS.memosList, (_e, q: ListQuery) => core.memos.list(q))
   ipcMain.handle(CHANNELS.memosCreate, (_e, i: CreateMemoInput) => core.memos.create(i))
   ipcMain.handle(CHANNELS.memosUpdate, (_e, i: UpdateMemoInput) => core.memos.update(i))
+  ipcMain.handle(
+    CHANNELS.memosUpdateIfUnchanged,
+    (_e, i: GuardedUpdateMemoInput) => core.memos.updateIfUnchanged(i)
+  )
   ipcMain.handle(CHANNELS.memosRemove, (_e, id: string) => core.memos.remove(id))
 }
